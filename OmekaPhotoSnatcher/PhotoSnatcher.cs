0c6d7219815aff60a0deb38c993374740b1c4446
craig <craig@CRAIG_PC>
2024-09-25 23:27:13 -0400

    Add project files.

---
+++ OmekaPhotoSnatcher/PhotoSnatcher.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OmekaPhotoSnatcher
+{
+    public class PhotoSnatcher
+    {
+        
+        static readonly HttpClient client = new HttpClient();
+
+        public static void Main(string filePath)
+        {
+            Logger.WriteLine("==========================================================");
+            Logger.WriteLine("Reading file " + filePath + "...\n");
+            Logger.SaveLog();
+            string jsonStringCollection = File.ReadAllText(filePath);
+            var oJson = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonStringCollection);
+            foreach (var item in oJson) 
+            {
+                string url = item["files"]["url"];
+                string filename = "Item_" + item["id"].ToString("0000") + ".jpg";
+                
+                GetApiResponse(url, filename).Wait();
+            }
+            
+        }
+        public static async Task GetApiResponse(string url, string filename)
+        {
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var oResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(responseBody);
+                string originalUrl = oResponse[0]["file_urls"]["original"];
+
+                SaveImage(originalUrl, filename).Wait();
+
+
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.Message == "Response status code does not indicate success: 429 (Too Many Requests).")
+                {
+                    Thread.Sleep(1000);
+                    GetApiResponse(url, filename).Wait();
+                }
+                else
+                {
+                    Logger.WriteLine("\nException Caught!");
+                    Logger.WriteLine("Message : " + e.Message);
+                    Logger.WriteLine(filename);
+                    Logger.SaveLog();
+                }
+                
+            }
+        }
+
+        public static async Task SaveImage(string url, string filename)
+        {
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new FileStream(filename, FileMode.Create))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }
+
+                    Logger.WriteLine("Image downloaded successfully: " + filename);
+                    Logger.SaveLog();
+                }
+                catch (HttpRequestException ex)
+                {
+                    Logger.WriteLine("Failed to download image: " + ex.Message);
+                    Logger.SaveLog();
+                }
+            }
+        }
+
+    }
+}
---
+++ OmekaPhotoSnatcher/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace OmekaPhotoSnatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("--------OMEKA PHOTO SNATCHER-------");
+            Console.WriteLine("-----------------------------------\n");
+
+            Console.WriteLine("This program will:");
+            Console.WriteLine("-Scan the .txt file you select for item numbers");
+            Console.WriteLine("-Call the Omeka API for getting files by item number");
+            Console.WriteLine("-Download all files with \"orignial\" tag");
+            Console.WriteLine("-Save the downloaded files as Item_#### in same folder where this program is located.");
+            Console.WriteLine("-Creates a log file in the same folder where this program is located\n");
+            
+
+
+
+            while (!endApp)
+            {
+                string? filePath;
+                string? runInput;
+                string? quitInput;
+
+                Console.WriteLine("Enter full file path and name:");
+                filePath = Console.ReadLine();
+
+                Console.Write("Run Program? Y/N: \n");
+                runInput = Console.ReadLine();
+                if (runInput == "Y" || runInput == "y") { PhotoSnatcher.Main(filePath); }
+
+                Console.Write("Quit? Y/N: \n");
+                quitInput = Console.ReadLine();
+                if (quitInput == "Y" || quitInput == "y") { endApp = true; }
+
+            }
+
+        }
+    }
+}
